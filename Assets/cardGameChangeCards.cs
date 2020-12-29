d55fd038f061bc5e109519406915746adf08a5ef
Ádám <mabolder@gmail.com>
2020-12-29 18:59:30 +0100

    Card game functions added

---
+++ Assets/cardGameChangeCards.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cardGameChangeCards : MonoBehaviour
+{
+     public Sprite card2;
+    // Start is called before the first frame update
+    void Start()
+    {
+    StartCoroutine (Test());
+    }    
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator Test(){
+        yield return new WaitForSeconds (2);
+        Debug.Log("Wait is over");
+        this.gameObject.GetComponent<Image>().sprite = card2;
+        Debug.Log("The actual image is:" + this.gameObject.GetComponent<Image>().sprite);
+    }
+
+}
---
+++ Assets/generateCards.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generateCards : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+public void generateNumber(){
+    //Debug.Log(UnityEngine.Random.Range(0,10));
+    List<int> cardList = new List<int>();
+
+    for (int i = 0; i < 10; i++){
+        int number = UnityEngine.Random.Range(0,10);
+        cardList.Add(number);
+    }
+    foreach(var number in cardList){
+    Debug.Log(number.ToString());
+    }
+    
+}
+
+}
