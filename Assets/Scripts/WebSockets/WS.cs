7263fcb149ae53bf9555a39b230f841c282f0445
mate.toth <mate.toth@stud.fh-campuswien.ac.at>
2021-02-13 20:03:40 +0100

    new address

--- Assets/Scripts/WebSockets/WS.cs
+++ Assets/Scripts/WebSockets/WS.cs
@@ -1,21 +1,17 @@
 ﻿
 using WebSocketSharp;
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
 
 namespace ConnectionNamespace
 {
     public class WS : MonoBehaviour
     {
-       public static WebSocket ws;
+        public static WebSocket ws;
         // Start is called before the first frame update
         void Start()
         {
-            ws = new WebSocket("ws://142.93.103.225/alienosauria");
+            ws = new WebSocket("ws://kutyadoki.hu/socket/");
             ws.Connect();
-          
-         
         }
 
         // Update is called once per frame
