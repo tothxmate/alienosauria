64c4131a16976034616a44694a546689ccff0ba4
Ádám Mózes <mabolder@gmail.com>
2021-02-18 17:19:37 +0100

    joingameblabla

--- Assets/Scripts/Navigation/joinGame.cs
+++ Assets/Scripts/Navigation/joinGame.cs
@@ -30,8 +30,8 @@ namespace ConnectionNamespace
             WS.role = "guest";
             WS.ws = new WebSocket("ws://kutyadoki.hu/socket/");
             WS.ws.Connect();
-            basicMessage msg = new basicMessage("setGuestUserId");
-            WS.ws.Send(JsonbasicUtility.ToJson(msg));
+            msg = "{\"action\":\"setGuestUserId\"}";
+            WS.ws.Send(msg);
             WS.ws.OnMessage += (sender, e) =>
             {
                 res = JsonUtility.FromJson<responseMessage>(e.Data);
@@ -48,11 +48,7 @@ namespace ConnectionNamespace
                         }
                         else
                         {
-<<<<<<< HEAD
-                            WS.userid_global = res.userid;
-=======
                             
->>>>>>> 0957ffd007c4a9df0471734b9838aca02d89f70f
                             Debug.Log("fucker");
                             popup p = new popup();
                             p.popupWindow("Please wait until the host starts the game!");
@@ -63,8 +59,8 @@ namespace ConnectionNamespace
                         nextScene = true;
                         
                         break;
-                    case "setGuestUserId"  :
-                    WS.userid_global = res.userid;  
+                    case "setGuestUserId":
+                        WS.userid_global = res.userid;  
                     break;
                 }
             };
@@ -98,7 +94,7 @@ namespace ConnectionNamespace
             if (roomNumber != "" && inGameName != "")
             {
             Debug.Log("ratyii");
-            msg = "{\"action\":\"initGuest\", \"username\":\"" + inGameName + "\",\"roomNumber\":\"" + roomNumber + "\"}";
+            msg = "{\"action\":\"initGuest\", \"username\":\"" + inGameName + "\",\"roomNumber\":\"" + roomNumber + "\",\"userid\":\"" + WS.userid_global + "\"}";
             WS.ws.Send(msg);
             Debug.Log("Waiting for host to start...");
             //SceneManager.LoadScene(3);
